ffd50cc852c9bd88f5fde65749775dd967bffcde
Emma Sharon <emmasharon19@gmail.com>
2019-05-06 17:05:07 +0300

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
 $(document).ready(function(){
-  $(
-    
-  )
+   $("#par1,#design").onclick(function(){
+       $("#par1").slideToggle();
+       $("#design").slideToggle();
+   });
+
+   $("#par2,#development").onclick(function(){
+       $("#par2").slideToggle();
+       $("#development").slideToggle();
+   });
+   $("#par3,#product").onclick(function(){
+       $("#par3").slideToggle();
+       $("#product").slideToggle();
+   });
 
 
-//   $('.A').hover(function () {
-//     $(this).animate({opacity:'0.9'});
-//   },
-//   function () {
-//     $(this).animate({opacity:'0'});
-//   }
-// });
+  $('.A').hover(function () {
+    $(this).animate({opacity:'0.9'});
+  },
+ function () {
+   $(this).animate({opacity:'0'});
+  }
+ });
   $(".A").hide();
   $(".view").hover(function(){
     $(this).children(".A").toggle();
