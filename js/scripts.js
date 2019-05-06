3575c1d7e91629e9045d85e449ecd3b277662cdd
Emma Sharon <emmasharon19@gmail.com>
2019-05-06 13:14:28 +0300

    create hover effect on container

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
-<div class="col-md-4 clickable">
-              <img src="imgs/design_icon.png">
-              <h5><strong>DESIGN</strong></h5>
-              <div class="hide">
-                      <p> Our design practice offers a full range of services including brand strategy,interaction and visual design and user experience testing.</p>
-                      <p>Throughout your project,our designers create and implement visual design and workflows,solicit user feedback and work with you to make sure what gets built is what is needed.</p>
-
-              </div>
-
-          </div>
-
-(.clickable).click(function(){
-(this).children('hide').toggle()})
+$(document).ready(function(){
+//   $('.A').hover(function () {
+//     $(this).animate({opacity:'0.9'});
+//   },
+//   function () {
+//     $(this).animate({opacity:'0'});
+//   }
+// });
+  $(".A").hide();
+  $(".view").hover(function(){
+    $(this).children(".A").toggle();
+  });
+  funt()
+});
